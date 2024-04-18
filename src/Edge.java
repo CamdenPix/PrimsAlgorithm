083e5134de6d513a6b67086cbd543825569e2008
CamdenPix <camdengenta@gmail.com>
2024-04-18 11:23:45 -0500

    Edge Class

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,20 @@
 public class Edge {
+    private int edge1;
+    private int edge2;
+    private float weight;
+    public Edge(int e1, int e2, float w){
+        edge1 = e1;
+        edge2 = e2;
+        weight = w;
+    }
+
+    public int getEdge1(){
+        return edge1;
+    }
+    public int getEdge2(){
+        return edge2;
+    }
+    public float getWeight(){
+        return weight;
+    }
 }
