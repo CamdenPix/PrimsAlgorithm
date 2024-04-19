d1ce972ec80b94f8d4e9c7bb87fb2a62fad3b714
CamdenPix <camdengenta@gmail.com>
2024-04-19 10:33:37 -0500

    Fixed DataReader issue

--- src/DataReader.java
+++ src/DataReader.java
@@ -1,19 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 public class DataReader {
     private File file;
     private ArrayList<Edge> edges = new ArrayList<Edge>(10);
     private int numOfEdges;
     public DataReader() throws FileNotFoundException {
-        file = new File("file:src/graph");
+        file = new File("src/graph");
         Scanner sc = new Scanner(file);
 
-        numOfEdges = sc.nextInt();
+        numOfEdges = Integer.parseInt(sc.nextLine());
         while (sc.hasNextLine()) {
             String s = sc.nextLine();
-            String[] inputs = s.split(" ");
+            String[] inputs = s.trim().split("\\s+");
 
             int v1 = Integer.parseInt(inputs[0]);
             int v2 = Integer.parseInt(inputs[1]);
